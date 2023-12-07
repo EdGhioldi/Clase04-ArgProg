d1f2039c11dac0d7282034545934b300c670601e
EdGhioldi <ramones_Rc@hotmail.com>
2023-12-07 20:23:37 -0300

    first commit

---
+++ src/main/java/org/example/estados/EstadoCancion.java
@@ -0,0 +1,5 @@
+package org.example.estados;
+
+public interface EstadoCancion {
+    public void definirEstado();
+}
---
+++ src/main/java/org/example/music/Cancion.java
@@ -0,0 +1,16 @@
+package org.example.music;
+import lombok.Data;
+
+@Data
+public class Cancion {
+    private String nombreCancion;
+    private Artista artista;
+    private Album album;
+
+    public Cancion(String nombreCancion, Artista artista, Album album){
+        this.nombreCancion = nombreCancion;
+        this.artista = artista;
+        this.album = album;
+    }
+
+}
