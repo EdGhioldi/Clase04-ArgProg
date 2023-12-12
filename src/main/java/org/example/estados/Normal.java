4daa0f1795a420b6d3f76bb96fefae13dd22d571
Eduardo Ghioldi <106218017+EdGhioldi@users.noreply.github.com>
2023-12-12 20:35:51 -0300

    Update Normal.java

--- src/main/java/org/example/estados/Normal.java
+++ src/main/java/org/example/estados/Normal.java
@@ -1,8 +1,14 @@
 package org.example.estados;
+import org.example.music.*;
 import lombok.Data;
 
 @Data
 public class Normal implements EstadoCancion{
-@Override
-public void definirEstado(){};
+
+    public Normal(){};
+    public void aNormal(Cancion cancion){
+        System.out.println("Ya se encuentra en el Estado 'Normal'");
+    };
+    public void aAuge(Cancion cancion){};
+    public void aTendencia(Cancion cancion){};
 }
