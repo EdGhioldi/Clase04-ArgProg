1af017e69a0f6c8b2ef4402338b64bcc60c1afb4
EdGhioldi <ramones_Rc@hotmail.com>
2023-12-12 15:35:49 -0300

    Creacion Clase ICONO

--- src/main/java/org/example/Icono.java
+++ src/main/java/org/example/Icono.java
@@ -4,7 +4,6 @@ public class Icono {
     public static Icono MUSICAL_NOTE = new Icono(new int[]{0xD83C, 0xDFB5});
     public static Icono ROCKET = new Icono(new int[]{0xD83D, 0xDE80});
     public static Icono FIRE = new Icono(new int[]{0xD83D, 0xDD25});
-
     private int[] internalEncoding;
 
     Icono(int[] internalEncoding) {
