f1feb88e5274b5990edd7bf14b437b0250985281
Eduardo Ghioldi <106218017+EdGhioldi@users.noreply.github.com>
2023-12-12 20:31:40 -0300

    Delete estados directory

--- estados/EstadoCancion.java
+++
@@ -1,7 +0,0 @@
-package org.example.estados;
-import org.example.music.Cancion;
-public interface EstadoCancion {
-    void aNormal(Cancion cancion);
-    void aAuge(Cancion cancion);
-    void aTendencia(Cancion cancion);
-}
