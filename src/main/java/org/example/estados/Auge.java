a60b1513f66b64fb4f997e0d657116726386646d
Eduardo Ghioldi <106218017+EdGhioldi@users.noreply.github.com>
2023-12-12 20:35:38 -0300

    Update Auge.java

--- src/main/java/org/example/estados/Auge.java
+++ src/main/java/org/example/estados/Auge.java
@@ -1,10 +1,15 @@
 package org.example.estados;
 import lombok.Data;
+import org.example.music.Cancion;
 
 @Data
 public class Auge implements EstadoCancion{
-    @Override
-    public void definirEstado() {
 
-    }
+    public void aNormal(Cancion cancion){};
+    public void aAuge(Cancion cancion){
+        System.out.println("Ya se encuentra en el Estado 'Auge'");
+    };
+    public void aTendencia(Cancion cancion){};
+
+
 }
