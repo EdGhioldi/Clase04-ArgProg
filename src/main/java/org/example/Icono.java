efaf1dbc512ba03f418a01e76071d689920b7673
EdGhioldi <ramones_Rc@hotmail.com>
2023-12-12 15:55:14 -0300

    Clase Icono / Correcion

--- src/main/java/org/example/Icono.java
+++ src/main/java/org/example/Icono.java
@@ -1,9 +1,9 @@
 package org.example;
 
 public class Icono {
-    public static Icono I_NORMAL = new Icono(new int[]{0xD83C, 0xDFB5});
-    public static Icono I_AUGE = new Icono(new int[]{0xD83D, 0xDE80});
-    public static Icono I_TENDENCIA = new Icono(new int[]{0xD83D, 0xDD25});
+    public static Icono MUSICAL_NOTE = new Icono(new int[]{0xD83C, 0xDFB5});
+    public static Icono ROCKET = new Icono(new int[]{0xD83D, 0xDE80});
+    public static Icono FIRE = new Icono(new int[]{0xD83D, 0xDD25});
     private int[] internalEncoding;
 
     Icono(int[] internalEncoding) {
