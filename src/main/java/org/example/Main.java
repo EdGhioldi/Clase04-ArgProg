94e40a8dbddc6d47dbca8d79aad38ebb12827047
Eduardo Ghioldi <106218017+EdGhioldi@users.noreply.github.com>
2023-12-13 20:36:21 -0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,16 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        Cancion Pinhead = new Cancion("Pinhead", "Ramones", "LeaveHome");
+
+        Pinhead.setLikes(50000);
+        Pinhead.setDisLikes(5);
+        Pinhead.setViews(500000);
+        Pinhead.setLanzamiento(1977);
+
+        Pinhead.aAuge(Pinhead);
+        Pinhead.show();
+
 
     }
 }
