1b7c239af0a54e1e9cea861f1c5d47d05d3a52cd
Eduardo Ghioldi <106218017+EdGhioldi@users.noreply.github.com>
2023-12-13 20:30:41 -0300

    Update Normal.java

--- src/main/java/org/example/estados/Normal.java
+++ src/main/java/org/example/estados/Normal.java
@@ -1,5 +1,6 @@
 package org.example.estados;
-import org.example.music.*;
+
+import org.example.*;
 import lombok.Data;
 
 @Data
@@ -9,6 +10,13 @@ public class Normal implements EstadoCancion{
     public void aNormal(Cancion cancion){
         System.out.println("Ya se encuentra en el Estado 'Normal'");
     };
-    public void aAuge(Cancion cancion){};
+    public void aAuge(Cancion cancion){
+        if(cancion.getViews()>1000 && cancion.getDisLikes() < 5000){
+            cancion.setEstado(new Auge());
+            cancion.setIcono(Icono.ROCKET);
+            cancion.setMensaje(cancion.getArtista() +  cancion.getNombreCancion()
+            + cancion.getAlbum() + cancion.getLanzamiento());
+        }
+    };
     public void aTendencia(Cancion cancion){};
 }
