7c438b731020aef0cbaf707ca9b8a69ccb82c62b
Eduardo Ghioldi <106218017+EdGhioldi@users.noreply.github.com>
2023-12-13 20:30:56 -0300

    Update Tendencia.java

--- src/main/java/org/example/estados/Tendencia.java
+++ src/main/java/org/example/estados/Tendencia.java
@@ -1,14 +1,21 @@
 package org.example.estados;
 
 import lombok.Data;
-import org.example.music.Cancion;
+import org.example.*;
+import java.time.LocalDate;
 
 @Data
 public class Tendencia implements  EstadoCancion{
-
-
-    public void aNormal(Cancion cancion){};
-    public void aAuge(Cancion cancion){};
+    public void aNormal(Cancion cancion){
+        if (cancion.getLastView() != null && cancion.getLastView().isEqual(LocalDate.now())) {
+            cancion.setEstado(new Normal());
+            cancion.setIcono(Icono.MUSICAL_NOTE);
+            System.out.println(cancion.getArtista() + " - " + cancion.getAlbum() + " - " + cancion.getNombreCancion());
+        }
+    };
+    public void aAuge(Cancion cancion){
+        System.out.println("No permitido.");
+    };
     public void aTendencia(Cancion cancion){
         System.out.println("Ya se encuentra en el Estado 'Tendencia'");
     };
