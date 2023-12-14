1d70633a6c5802a3be239556642a602e8d55bde7
Eduardo Ghioldi <106218017+EdGhioldi@users.noreply.github.com>
2023-12-14 17:13:34 -0300

    Update Tendencia.java

--- src/main/java/org/example/estados/Tendencia.java
+++ src/main/java/org/example/estados/Tendencia.java
@@ -6,17 +6,23 @@ import java.time.LocalDate;
 
 @Data
 public class Tendencia implements  EstadoCancion{
+
+    public Tendencia(){};
     public void aNormal(Cancion cancion){
-        if (cancion.getLastView() != null && cancion.getLastView().isEqual(LocalDate.now())) {
+        if (cancion.getViews()<1000 && cancion.getLikes()<20000 ||
+            cancion.getDisLikes()>5000){
             cancion.setEstado(new Normal());
             cancion.setIcono(Icono.MUSICAL_NOTE);
-            System.out.println(cancion.getArtista() + " - " + cancion.getAlbum() + " - " + cancion.getNombreCancion());
+            cancion.setMensaje(cancion.getArtista() + " - " + cancion.getAlbum() + " - " + cancion.getNombreCancion());
+            cancion.show();
+        }else{
+            System.out.println("Imposible, está CANCION NO ESTÁ EN EL ESTADO SELECCIONADO");
         }
     };
     public void aAuge(Cancion cancion){
-        System.out.println("No permitido.");
+        System.out.println("Imposible, esta CANCION NO ESTÁ EN AUGE!");
     };
     public void aTendencia(Cancion cancion){
-        System.out.println("Ya se encuentra en el Estado 'Tendencia'");
+            System.out.println("Ya se encuentra en el Estado 'Tendencia'");
     };
 }
