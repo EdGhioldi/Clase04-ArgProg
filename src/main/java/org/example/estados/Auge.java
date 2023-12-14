974035741ca525e936998f08d6b2e56366fa285f
Eduardo Ghioldi <106218017+EdGhioldi@users.noreply.github.com>
2023-12-14 17:13:13 -0300

    Update Auge.java

--- src/main/java/org/example/estados/Auge.java
+++ src/main/java/org/example/estados/Auge.java
@@ -2,16 +2,40 @@ package org.example.estados;
 
 import lombok.Data;
 import org.example.Cancion;
+import org.example.Icono;
+
+import java.time.LocalDate;
 
 @Data
 public class Auge implements EstadoCancion{
 
-    public void aNormal(Cancion cancion){};
-    public void aAuge(Cancion cancion){
+    public Auge(){};
+
+    public void aNormal(Cancion cancion){
+        if      (cancion.getViews() < 1000
+                || cancion.getLikes() < 20000
+                || cancion.getDisLikes() > 5000) {
+            cancion.setEstado(new Normal());
+            cancion.setIcono(Icono.MUSICAL_NOTE);
+            cancion.setMensaje(cancion.getArtista() + " - " + cancion.getAlbum() + " - " + cancion.getNombreCancion());
+            cancion.show();
+        }else{
+            System.out.println("Imposible, está CANCION NO ESTÁ EN EL ESTADO SELECCIONADO");
+        }
+    };    public void aAuge(Cancion cancion){
         System.out.println("Ya se encuentra en el Estado 'Auge'");
     };
-    public void aTendencia(Cancion cancion){};
-
+    public void aTendencia(Cancion cancion){
+        if((cancion.getLastView() != null || !cancion.getLastView().isBefore(LocalDate.now().minusDays(1)))
+        && cancion.getViews()>50000 && cancion.getLikes()>20000 && cancion.getDisLikes()<5000){
+            cancion.setEstado(new Tendencia());
+            cancion.setIcono(Icono.FIRE);
+            cancion.setMensaje(cancion.getNombreCancion() + " - " + cancion.getArtista() + " - " +
+                    cancion.getAlbum() + " - " + cancion.getLanzamiento());
+            cancion.show();
+        }else{
+            System.out.println("Imposible, esta canción NO ES TENDENCIA!");
+        }
+    };
 
 }
-
