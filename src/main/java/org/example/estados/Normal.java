327f9778878b7915a8eb603b3d58760e0c69b988
Eduardo Ghioldi <106218017+EdGhioldi@users.noreply.github.com>
2023-12-14 17:12:59 -0300

    Update Normal.java

--- src/main/java/org/example/estados/Normal.java
+++ src/main/java/org/example/estados/Normal.java
@@ -3,21 +3,39 @@ package org.example.estados;
 import org.example.*;
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class Normal implements EstadoCancion{
 
     public Normal(){};
     public void aNormal(Cancion cancion){
-        System.out.println("Ya se encuentra en el Estado 'Normal'");
+        System.out.println("Ya se encuentra en Estado 'Normal'");
+        cancion.show();
     };
     public void aAuge(Cancion cancion){
-        if(cancion.getViews()>1000 && cancion.getDisLikes() < 5000){
+        if((cancion.getLastView() != null && !cancion.getLastView().isBefore(LocalDate.now().minusDays(1)))
+                && cancion.getViews()>1000 && cancion.getViews() < 50000 && cancion.getDisLikes() < 5000){
             cancion.setEstado(new Auge());
             cancion.setIcono(Icono.ROCKET);
             cancion.setMensaje(cancion.getArtista() + " - " +  cancion.getNombreCancion()
-            + " - " + cancion.getAlbum() + " - " + cancion.getLanzamiento());
+                    + " - " + cancion.getAlbum() + " - " + cancion.getLanzamiento());
+            cancion.show();
+
+        }else{
+            System.out.println("Imposible, esta CANCION NO ESTÁ EN AUGE!");
         }
     };
-    public void aTendencia(Cancion cancion){};
+    public void aTendencia(Cancion cancion){
+        if((cancion.getLastView() != null && !cancion.getLastView().isBefore(LocalDate.now().minusDays(1))
+            && cancion.getViews()>50000 && cancion.getLikes()>20000)){
+            cancion.setEstado(new Tendencia());
+            cancion.setIcono(Icono.FIRE);
+            cancion.setMensaje(cancion.getNombreCancion() + " - " + cancion.getArtista() + " - " +
+                    cancion.getAlbum() + " - " + cancion.getLanzamiento());
+            cancion.show();
+        }else{
+            System.out.println("Imposible, esta canción NO ES TENDENCIA!");
+        }
+    };
 }
-
